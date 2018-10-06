a226678b2f97a7c53f2f0d85cee1da5876ddb58f
Isabel Tiburski <itiburski@gmail.com>
2018-10-06 10:29:31 -0300

    Add tests for DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get('/todos/:id', (req, res) => {
   let id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
-    return res.status(412).send();
+    return res.status(404).send();
   }
 
   Todo.findById(id).then((todo) => {
